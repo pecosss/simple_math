f6713946c73adc2eab3577d26cf4d7c90dfa7538
Alessio Pecorari <alessio.p2005@gmail.com>
2026-03-03 12:46:58 +0100

    Gestione numeri complessi, da testare.

--- simple_math.c
+++ simple_math.c
@@ -13,3 +13,22 @@ int sum(int x, int y){
 float avg(int x, int y){
     return (sum(x,y)/2.0);
 }
+
+complex sum_cmplx(complex x, complex y){
+    float sum_Re = x.Re + y.Re;
+    float sum_Im = x.Im + y.Im;
+
+    complex res;
+    res.Re = sum_Re;
+    res.Im = sum_Im;
+
+    return res;
+}
+
+complex avg_cmplx(complex x, complex y){
+    complex res = sum_cmplx(x, y);
+    res.Re = res.Re / 2.0;
+    res.Im = res.Im / 2.0;
+
+    return res;
+}
--- simple_math.h
+++ simple_math.h
@@ -6,9 +6,25 @@
 // necessario per evitare che il pre-processore crei doppioni di definizioni
 #ifndef _SIMPLEMATH_
     #define _SIMPLEMATH_
-        // sum: input (2 integers) - output (1 integer)
-        int sum(int x, int y);
 
-        // avg: input (2 integers) - output (1 float)
-        float avg(int x, int y);
+    // struct: una semplificazione di oggetto
+    typedef struct complex_number
+    {
+        float Re;
+        float Im;
+    } complex;
+
+    // sum: input (2 integers) - output (1 integer)
+    int sum(int x, int y);
+
+    // avg: input (2 integers) - output (1 integer)
+    float avg(int x, int y);
+
+    // sum_cmplx: input (2 complex numbers) - output (1 complex number)
+    // in C è necessario definire le funzioni con nomi diversi, nonostante la differenza di tipo
+    complex sum_cmplx(complex x, complex y);
+
+    // avg_cmplx: input (2 complex numbers) - output (1 complex number)
+    complex avg_cmplx(complex x, complex y);
+
 #endif
